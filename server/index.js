d1dececf81caf5e9c414f8f7db4ff86b3c7727dc
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-03-02 22:24:30 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
 import express from "express";
 import emailRouter from "./routes/EmailRoute.js";
-import cors from "cors"
+import cors from "cors";
 import leetcodeRoute from "./routes/leetcodeRoute.js";
 import CFroute from "./routes/codeforcesRoute.js";
 import githubRoute from "./routes/githubRoute.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN, // frontend URL
+    origin: [
+      process.env.FRONTEND_ORIGIN,
+      "https://portfolio-wheat-gamma-90.vercel.app/",
+    ],
     credentials: true,
   }),
 );
 
 app.use("/api", emailRouter);
-app.use("/api" , leetcodeRoute)
-app.use("/api" , CFroute)
-app.use("/api", githubRoute)
+app.use("/api", leetcodeRoute);
+app.use("/api", CFroute);
+app.use("/api", githubRoute);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("Server Running on port :" , port);
+  console.log("Server Running on port :", port);
 });
